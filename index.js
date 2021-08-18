fde17830f77ab68a16d0fc44c9aa9cc0dd47f774
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-18 19:45:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let {
   WAConnection
 } = require('@adiwajshing/baileys')
+let axios = require('axios')
 
 async function connectToWhatsApp () {
   const mut = new WAConnection()
+  mut.on('chats-received', async ({ hasNewChats }) => {
+    console.log(`You have ${mut.chats.length} chats, Chats available: ${hasNewChats}`)
+
+    const unread = await mut.loadAllUnreadMessages ()
+    console.log ("you have " + unread.length + " unread messages")
+  })
+  mut.on('contacts-received', () => {
+    console.log('you have ' + Object.keys(mut.contacts).length + ' contacts')
+  })
 }
