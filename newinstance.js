c97a3ddc398394dbd46f784b45ecbed339a7e78a
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-19 23:04:18 +0700

    Fix includes

--- newinstance.js
+++ newinstance.js
@@ -4,20 +4,25 @@ let {
 let fs = require('fs')
 let fetch = require('node-fetch')
 
-//Function
+const newinstance = async ({ webhook }) => {
 async function startBot () {
   const MUT = new WAConnection()
   let attempts = 0
   MUT.on('qr', qr => {
     attempts += 1
-    body: JSON.stringify({ type: 'qr', qr, attempts })
+    fetch(webhook, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ type: 'qr', qr, attempts })
+    }).catch(() => {})
   })
   MUT.on('blocklist-update', () => {
     console.log('blocklist-update')
     const number = MUT.user.jid.split('@s.whatsapp.net')[0]
     fs.writeFileSync(`auth_info/${number}.json`, JSON.stringify(MUT.base64EncodedAuthInfo(), null, 2))
   })
-
   MUT.on('received-pong', () => {
     console.log('received-pong')
     MUT.close()
