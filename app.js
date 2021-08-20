006bc3c9b17c6b634479cc37a8694af5004cae50
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-20 08:53:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const { WAConnection } = require('@adiwajshing/baileys')
 const fs = require('express')
 const newinstance = require('./newinstance')
+const message = require('./lib/msg/message')
 
 //function
 async function startBot () {
   const MUT = new WAConnection()
+  //Calling files
   return newinstance()
+  return message()
 }
 
 startBot()
