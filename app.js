9f5264545df75afe3323f6e7524c5101d38391ae
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-27 20:54:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const { WAConnection } = require('@adiwajshing/baileys')
 const fs = require('fs')
-const newinstance = require('./newinstance')
 const message = require('./lib/msg/message')
 const axios = require('axios')
-const { groupart } = require('./lib/function/function')
+
+blocked = []
+
 //function
 async function startBot () {
   const MUT = new WAConnection()
+  let { start, info, success, close } = require('./lib/instance')
+  const MUT = new WAConnection()
+  MUT.logger.level = 'warn'
+  MUT.on('qr', qr => {
+    console.log('[server] scan the qr code')
+  })
+  MUT.on('credentials-updated', () => {
+    fs.writeFileSync('./auth_info/auth.json', JSON.stringify(MUT.base64EncodedAuthInfo(), null, '\t'))
+    info('2', 'Login Info Updated')
+  })
+  fs.existsSync('./auth_info/auth.json') && MUT.loadAuthInfo('./auth_info/auth.json')
+  MUT.on('connecting', () => {
+    start('2', 'Connecting...')
+  })
+  MUT.on('open', () => {
+    success('2', 'Connected')
+  })
+  MUT.on('CB:Blocklist', json => {
+    if (blocked.length > 2) return
+    for (let i of json[1].blocklist) {
+      blocked.push(i.replace('c.us','s.whatsapp.net'))
+    }
+  })
+  await  MUT.connect()
   //Calling files
-  return newinstance()
-  return groupart(MUT)
-  return message(MUT)
 }
 
 startBot()
