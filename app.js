87936b6f2545cd081595fe00aeafb3ef67664929
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-27 08:40:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const { WAConnection } = require('@adiwajshing/baileys')
 const fs = require('fs')
 const newinstance = require('./newinstance')
 const message = require('./lib/msg/message')
-
+const axios = require('axios')
+const { groupart } = require('./lib/function/function')
 //function
 async function startBot () {
   const MUT = new WAConnection()
   //Calling files
   return newinstance()
-  return message()
+  return groupart(MUT)
+  return message(MUT)
 }
 
 startBot()
