2b53a4f31791b20077d89dbe2057a8b5193ccddb
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-27 20:58:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,40 @@ async function startBot () {
   MUT.on('open', () => {
     success('2', 'Connected')
   })
+  MUT.on('group-participants-update', async (anu) => {
+    //if (!welkom.includes(anu.jid)) return
+    try {
+      let gcind = require('./../msg/lang/ind')
+      let gceng = require('./../msg/lang/eng')
+      let gcset = JSON.parse(fs.readFileSync('./../setting.json'))
+      let gclang = 'gc' + gcset.language
+      const mdata = await MUT.groupMetadata(anu.jid)
+      console.log(anu)
+      if (anu.action == 'add') {
+	num = anu.participants[0]
+	try {
+	  ppimg = await MUT.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
+	} catch {
+	  ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+	}
+	teks = gclang(sayhello()) + `@${num.split('@')[0]}\n` + gclang(gcwelcome()) + `*${mdata.subject}*`
+	let buff = await getBuffer(ppimg)
+	MUT.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
+      } else if (anu.action == 'remove') {
+	num = anu.participants[0]
+	try {
+	  ppimg = await MUT.getProfilePicture(`${num.split('@')[0]}@c.us`)
+	} catch {
+          ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+	}
+	teks = gclang(saygoodbye()) + `@${num.split('@')[0]}👋`
+	let buff = await getBuffer(ppimg)
+        MUT.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
+      }
+    } catch (e) {
+      console.log('Error : %s', color(e, 'red'))
+    }
+  })
   MUT.on('CB:Blocklist', json => {
     if (blocked.length > 2) return
     for (let i of json[1].blocklist) {
