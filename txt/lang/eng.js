f69af9240aa4a0815b40a50be2623e70661889f1
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-31 02:21:07 +0700

    Create eng.js

---
+++ txt/lang/eng.js
@@ -0,0 +1,34 @@
+exports.help = (prefix) => {
+  return `
+[Main]
+=> ${prefix}help
+=> ${prefix}menu
+
+[Fun]
+=> ${prefix}meme
+`
+}
+exports.wait = () => {
+  return `Wait...`
+}
+exports.hours = () => {
+  return `Hours`
+}
+exports.minute = () => {
+  return `Minute`
+}
+exports.second = () => {
+  return `Second`
+}
+exports.hello = () => {
+  return `Hello`
+}
+exports.welcome = () => {
+  return `Welcome to`
+}
+exports.goodbye = () => {
+  return `GoodBye`
+}
+exports.done = () => {
+  return `Done`
+}
