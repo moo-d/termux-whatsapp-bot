3d3f131126c446aead9f9ebfcd04abc923183550
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-08-31 02:13:32 +0700

    Create ind.js

---
+++ txt/lang/ind.js
@@ -0,0 +1,18 @@
+exports.wait = () => {
+  return `Mohon tunggu sebentar...`
+}
+exports.hours = () => {
+  return `Jam`
+}
+exports.minute = () => {
+  return `Menit`
+}
+exports.second = () => {
+  return `Detik`
+}
+exports.hello = () => {
+  return `Halo`
+}
+exports.welcome = () => {
+  return `Selamat datang di`
+}
