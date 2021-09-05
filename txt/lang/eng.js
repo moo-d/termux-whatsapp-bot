590fd8e6ada2810f12ff8adcb542e026a2555326
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-09-05 23:43:41 +0700

    Update eng.js

--- txt/lang/eng.js
+++ txt/lang/eng.js
@@ -32,3 +32,18 @@ exports.goodbye = () => {
 exports.done = () => {
   return `Done`
 }
+exports.onlygroup = () => {
+  return `[ ! ] This command can only be used in groups.`
+}
+exports.onlyadmin = () => {
+  return `[ ! ] This command can only be used by group admins.`
+}
+exports.onlybadmin = () => {
+  return `[ ! ] This command can only be used when the bot is an admin group.`
+}
+exports.acc = () => {
+  return `Order accepted.`
+}
+exports.addedadmin = () => {
+  return `add to be admin group.`
+}
