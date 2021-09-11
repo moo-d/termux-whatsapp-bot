2130d83e576674f30382368c7700bacdfdf79319
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-09-11 17:41:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 
-echo "this code is not finished"
+echo "[Installing Package]"
+apt-get install -y nodejs libwebp ffmpeg wget tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+echo "[Installing Dependencies]"
+npm install
+echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
