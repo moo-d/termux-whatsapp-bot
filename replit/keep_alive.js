3a221ac79e633f47d383503572d19877e9b7a682
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-09-14 18:45:19 +0700

    Update keep_alive.js

--- replit/keep_alive.js
+++ replit/keep_alive.js
@@ -4,8 +4,5 @@ let SocketIO = require('socket.io');
 let qrcode = require('qrcode');
 let app = new express();
 let PORT = process.env.PORT || 3000;
-let fs = require('fs');
-let setting = JSON.parse(fs.readFileSync('./lib/setting.json'))
-let prefix = new setting.prefix
 
 app.use(express.static(path.join(__dirname, 'views')))
