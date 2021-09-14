792118798e75192eab9d6dc33b3b8f3df2a55182
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-09-14 18:44:40 +0700

    Update keep_alive.js

--- replit/keep_alive.js
+++ replit/keep_alive.js
@@ -1,4 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
+let express = require('express');
+let path = require('path');
+let SocketIO = require('socket.io');
+let qrcode = require('qrcode');
+let app = new express();
+let PORT = process.env.PORT || 3000;
+let fs = require('fs');
+let setting = JSON.parse(fs.readFileSync('./lib/setting.json'))
+let prefix = new setting.prefix
+
+app.use(express.static(path.join(__dirname, 'views')))
