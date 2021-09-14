991a96f15b34bae7399402185ad4645ef0533daf
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-09-14 18:45:39 +0700

    Update keep_alive.js

--- replit/keep_alive.js
+++ replit/keep_alive.js
@@ -1,7 +1,5 @@
 let express = require('express');
 let path = require('path');
-let SocketIO = require('socket.io');
-let qrcode = require('qrcode');
 let app = new express();
 let PORT = process.env.PORT || 3000;
 
