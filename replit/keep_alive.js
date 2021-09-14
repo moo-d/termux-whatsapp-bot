e5241fdad41fada19e1e4ec788f83b0370717afe
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-09-14 19:25:27 +0700

    Update keep_alive.js

--- replit/keep_alive.js
+++ replit/keep_alive.js
@@ -4,3 +4,4 @@ let app = new express();
 let PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'views')))
+app.listen(PORT, () => console.log('App listened on port', PORT))
